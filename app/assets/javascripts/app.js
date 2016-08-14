79db86d9902eaa8c12be92e37ab89a3964e56609
Paul Stone <pmstone247@gmail.com>
2016-08-13 19:15:32 -0600

    CSS updates; renamed mainCtrl

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,15 +1,15 @@
 angular.module('projectDj', ['ui.router', 'templates'])
-.config([
-'$stateProvider',
-'$urlRouterProvider',
-function($stateProvider, $urlRouterProvider) {
+  .config([
+    '$stateProvider',
+    '$urlRouterProvider',
+    function($stateProvider, $urlRouterProvider) {
 
-  $stateProvider
-    .state('home', {
-      url: '/home',
-      templateUrl: 'home/_home.html',
-      controller: 'MainCtrl'
-    });
+      $stateProvider
+        .state('home', {
+          url: '/home',
+          templateUrl: 'home/_home.html',
+          controller: 'HomeCtrl'
+        });
 
-  $urlRouterProvider.otherwise('home');
-}]);
+      $urlRouterProvider.otherwise('home');
+    }]);
